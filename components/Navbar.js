5f555da8d337a16078ae030ba45dd81ecd3a6884
Vaibhav Singh Bhadouria <86617405+Vs153698@users.noreply.github.com>
2022-01-05 12:04:10 +0530

    Navbar Completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,35 @@
+import Image from 'next/image';
+import styles from '../styles/Navbar.module.css'
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <div className={styles.callbutton}>
+                    {/* if we are using Image container width and height are necessary  */}
+                    <Image src='/img/telephone.png' alt='' width="32" height="32" />
+                </div>
+                <div className={styles.texts}>
+                    <div className={styles.text}>ORDER NOW !</div>
+                    <div className={styles.text}>012 345 678</div>
+                </div>
+            </div>
+            <div className={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.listitem}>Homepage</li>
+                    <li className={styles.listitem}>Products</li>
+                    <li className={styles.listitem}>Menu</li>
+                    <h1 className={styles.mainlogo}>PizzaBar</h1>
+                    <li className={styles.listitem}>Events</li>
+                    <li className={styles.listitem}>Blog</li>
+                    <li className={styles.listitem}>Contacts</li>
+                </ul>
+            </div>
+            <div className={styles.cart}>
+                <div>
+                    <Image src='/img/cart.png' alt="" width="30px" height='30px'/>
+                </div>
+                <div className={styles.counter}>2</div>
+            </div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Pizza-Home</title>
         <meta name="description" content="Best pizza delivering services in town" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@500&family=Lobster&family=Pacifico&family=Permanent+Marker&display=swap" rel="stylesheet"/>
       </Head>
   
       Home
