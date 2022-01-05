30cfd8d3fff625cae440fb426d6e895fda56ebb4
Vaibhav Singh Bhadouria <86617405+Vs153698@users.noreply.github.com>
2022-01-05 17:14:08 +0530

    PizzaList Completed 05/01/2022

--- components/PizzaCard.js
+++ components/PizzaCard.js
@@ -1,7 +1,14 @@
-const PizzaCard = () => {
+import Image from 'next/image';
+import styles from '../styles/PizzaCard.module.css'
+const PizzaCard = (props) => {
     return (
-        <div>
-            pizza card
+        <div className={styles.container}>
+            <Image src={props.img} alt='' width='500' height='500'/>
+            <div className={styles.title}>FIORI DI ZUCA</div>
+            <span className={styles.price}>$19.90</span>
+            <div className={styles.desc}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </div>
         </div>
     );
 }
--- components/PizzaList.js
+++ components/PizzaList.js
@@ -1,13 +1,23 @@
 import styles from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard';
 const PizzaList = () => {
     return (
         <div className={styles.container}>
            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
            <p className={styles.desc}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae incidunt id odio repellat itaque est ratione suscipit magnam exercitationem laboriosam facilis modi porro, ea iusto voluptatem aperiam sapiente blanditiis facere.
+           “Anyone who says that money cannot buy happiness has clearly never spent their money on pizza.”
            </p>
            <div className={styles.wrapper}>
-               
+               <PizzaCard img='/img/pizza.png'/>
+               <PizzaCard img='/img/p1.png'/>
+               <PizzaCard img='/img/p3.png'/>
+               <PizzaCard img='/img/p4.png'/>
+               <PizzaCard img='/img/p22.png'/>
+               <PizzaCard img='/img/pn.png'/>
+               <PizzaCard img='/img/p3.png'/>
+               <PizzaCard img='/img/p4.png'/>
+               <PizzaCard img='/img/p1.png'/>
+               <PizzaCard img='/img/p22.png'/>
            </div>
         </div>
     );
